99981ff5aa2ed0732600407dc1d867cf161ca0fa
So-ko-nyun <cn6867@naver.com>
2025-06-05 14:22:46 +0900

    [chore] [BVFH-168] CI/CD Test

--- src/main/java/org/example/searchservice/Product.java
+++ src/main/java/org/example/searchservice/Product.java
@@ -1,13 +1,13 @@
-package org.example.searchservice;
-
-import org.springframework.data.annotation.Id;
-import org.springframework.data.elasticsearch.annotations.Document;
-
-@Document(indexName = "product")
-public class Product {
-    @Id
-    private String id;
-
-    private String name;
-    private String description;
-}
+//package org.example.searchservice;
+//
+//import org.springframework.data.annotation.Id;
+//import org.springframework.data.elasticsearch.annotations.Document;
+//
+//@Document(indexName = "product")
+//public class Product {
+//    @Id
+//    private String id;
+//
+//    private String name;
+//    private String description;
+//}
